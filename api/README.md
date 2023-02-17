e025db10a030ab3cdaf355a3d11cc3ca03e02df6
Roberto Zanin <raszanin@gmail.com>
2023-02-17 15:56:10 -0300

    mongodb + models

---
+++ api/README.md
@@ -0,0 +1,3 @@
+# Docker
+
+- docker run --name mongo -p 27017:27017 -d mongo
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,13 +1,24 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express();
+mongoose
+  .set('strictQuery', false)
+  .connect('mongodb://localhost:27017')
+  .then(() => {
+    const app = express();
 
-const port = 3333;
+    const port = 3333;
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Waiter App - Server Online' });
-});
+    app.get('/', (request, response) => {
+      return response.json({ message: 'Waiter App - Server Online' });
+    });
 
-app.listen(port, () => {
-  console.log(`🚀 WaiterApp Server is online on port http://localhost:${port}`);
-});
+    app.listen(port, () => {
+      console.log(
+        `🚀 WaiterApp Server is online on port http://localhost:${port}`
+      );
+    });
+  })
+  .catch(() => {
+    console.log('⚠️ Não foi possível conectar ao mongo');
+  });
